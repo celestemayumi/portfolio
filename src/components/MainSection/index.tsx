920c01f5c9589364ac5aa448ea29b306ba0545cb
Celeste Tanaka <cehmayumi@gmail.com>
2024-04-25 20:42:11 -0300

    first commit

---
+++ src/components/MainSection/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const MainSection = () => {
+  return (
+    <section>
+      <h1 className="text-black mb-4 text-6xl font-bold">Celeste Tanaka</h1>
+      <p className="text-black">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius veritatis
+        reprehenderit enim pariatur tenetur, harum, voluptatibus, cum sapiente
+        officiis rerum distinctio natus neque quos consectetur quae dicta quam
+        quia vero!
+      </p>
+    </section>
+  );
+};
+
+export default MainSection;
