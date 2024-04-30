1edce163cc80c72ba13e32d7b5f0f62527ba8019
Celeste Tanaka <cehmayumi@gmail.com>
2024-04-30 11:40:23 -0300

    ✨ feat: tech skills icons

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -5,43 +5,56 @@ import Link from "next/link";
 const About = () => {
   return (
     <>
-      <h1 id="about" className="text-4xl text-[#592e6d] font-bold oxygen-mono ml-20">
+      <h1
+        id="about"
+        className="text-4xl text-[#592e6d] font-bold oxygen-mono ml-20"
+      >
         &lt;Sobre/&gt;
       </h1>
-      <div className="box flex items-center justify-evenly p-10" >
+      <div className="box flex items-center justify-evenly p-10">
         <div className="flex flex-col items-center">
           <Image
-            className="rounded-xl"
+            className="rounded-full"
             src="/pfp.svg"
             width={300}
             height={300}
             alt="Imagem de perfil"
           />
-          <Link href={"mailto:celestetanakaa@gmail.com"} className="inline-block">
-            <div className="p-5 hover:scale-105 transition-transform">
-              <h1 className="text-lg font-semibold text-black hover:none">Entre em contato!</h1>
+          <Link href={"mailto:celestetanakaa@gmail.com"}>
+            <div className="p-5 mt-5 hover:scale-105 transition-transform">
+              <h1 className="text-2xl font-semibold text-black hover:none">
+                Entre em contato!
+              </h1>
               <div className="w-full h-2 bg-[#592e6d]  rounded-full -translate-x-2"></div>
               <div className="w-full h-2 bg-[#51cacc]  rounded-full translate-x-2"></div>
             </div>
           </Link>
         </div>
         <div className="w-3/6 text-black border-solid	border p-10 border-[#bdbdbd] rounded-xl ">
-          <h1 className="montserrat text-2xl font-bold mb-5 text-[#51cacc] ">
+          <h1 className="montserrat text-2xl font-bold text-black">
             Quem sou eu?
           </h1>
+          <div className="w-1/3 h-1 bg-[#592e6d]  rounded-full"></div>
+          <div className="w-1/3 h-1 mb-5 bg-[#51cacc]  rounded-full translate-x-2"></div>
           <p className="poppins text-lg font-medium leading-7">
-            Olá! Me chamo Celeste e sou estudante de <span className="text-[#cb6869]">Análise e Desenvolvimento de Sistemas</span> na FIAP e dev
-            <span className="text-[#cb6869]"> full-stack</span> com um toque
-            especial por <span className="text-[#cb6869]">front-end</span>.
+            Olá! Me chamo Celeste e sou estudante de
+            <span className="text-[#c43b67] font-semibold">
+               Análise e Desenvolvimento de Sistemas </span>
+             na FIAP e dev
+            <span className="text-[#c43b67] font-semibold"> full-stack</span> com um toque
+            especial por <span className="text-[#c43b67] font-semibold">front-end</span>.
             Atualmente estou aprendendo mais sobre
-            <span className="text-[#cb6869]"> back-end</span> e
-            <span className="text-[#cb6869]"> ciência de dados</span>!
+            <span className="text-[#c43b67] font-semibold"> back-end</span> e
+            <span className="text-[#c43b67] font-semibold"> ciência de dados</span>!
             <br />
             <br />
-            Fui introduzida a área em 2021 com cursos de <span className="text-[#cb6869]">HTML</span>, <span className="text-[#cb6869]">CSS</span>, <span className="text-[#cb6869]">Javascript</span> e
-            <span className="text-[#cb6869]"> Vue.js</span> pela Udemy em 2021 durante a pandemia. Desde então me
-            apaixonei por desenvolvimento e resolvi me aprofundar mais começando
-            minha graduação.
+            Fui introduzida a área em 2021 com cursos de
+            <span className="text-[#c43b67] font-semibold"> HTML</span>,
+            <span className="text-[#c43b67] font-semibold"> CSS</span>,
+            <span className="text-[#c43b67] font-semibold"> Javascript</span> e
+            <span className="text-[#c43b67] font-semibold"> Vue.js</span> pela Udemy em 2021
+            durante a pandemia. Desde então me apaixonei por desenvolvimento e
+            resolvi me aprofundar mais começando minha graduação.
             <br />
             <br />
             Busco criar experiências digitais que encantem e inspirem, adoro
--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,9 +1,23 @@
+import skills from "../../data/skills.json"
+import Image from "next/image"
+
 const Skills = ()=>{
     return(
         <>
-        <div className="w-3/6 p-16 text-black">
+        <div id='#skills' className="w-3/6 p-16 text-black">
         <h1>Skills</h1>
-        <p>a</p>
+        <div>
+            <div className="">
+            {skills.map((skill, id) => {
+                return(
+                    <div key={id}>
+                        <div><Image src={skill.imgSrc} width={50} height={50} alt="Icone da tecnologia"></Image></div>
+                        <p>{skill.title}</p>
+                    </div>
+                )
+            })}
+            </div>
+        </div>
         </div>
         </>
     )
