c04c7d53351b36d66b2830bd5c88514729124456
Celeste Tanaka <cehmayumi@gmail.com>
2024-05-14 08:26:38 -0300

    ✨  feat: skills scroll animation

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,5 +1,20 @@
 import skills from "../../data/skills.json"
 import Image from "next/image"
+import { motion } from "framer-motion"
+
+const fadeVariants = {
+    initial: {
+        opacity: 0,
+        y: 100
+    },
+    animate: (id:number)=>({
+        opacity: 1,
+        y:0,
+        transition: {
+            delay: 0.05 * id
+        }
+    })
+}
 
 const Skills = ()=>{
     return(
@@ -10,10 +25,10 @@ const Skills = ()=>{
             <div className="flex flex-wrap gap-3 justify-center">
             {skills.map((skill, id) => {
                 return(
-                    <div className='skill flex flex-col items-center gap-3'key={id}>
+                    <motion.div className='skill flex flex-col items-center gap-3'key={id} variants={fadeVariants} initial="initial" whileInView="animate" custom={id}>
                         <div className="bg-[#dcdcdc] rounded-full flex items-center justify-center m-3 p-3 w-28 h-28 hover:scale-110 transition-transform"><Image src={skill.imgSrc} width={60} height={50} alt="Icone da tecnologia"></Image></div>
                         <p className="font-bold tracking-wide">{skill.title}</p>
-                    </div>
+                    </motion.div>
                 )
             })}
             </div>
