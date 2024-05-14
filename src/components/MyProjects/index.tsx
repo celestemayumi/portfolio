408534df60488b8b8ce5cb0a90a0b01d04d2228b
Celeste Tanaka <cehmayumi@gmail.com>
2024-05-14 14:09:02 -0300

    💫 feat: animation project card

--- src/components/MyProjects/index.tsx
+++ src/components/MyProjects/index.tsx
@@ -1,44 +1,57 @@
 import projects from "../../data/projects.json";
 import Image from "next/image";
+import { useScroll, motion } from "framer-motion";
+import { useRef } from "react";
 
 const Projects = () => {
+  const ref = useRef(null)
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["1 5", "0.1 1"]
+  })
   return (
     <>
       <div id="projects" className="mt-10">
-        <h1 className="text-4xl text-[#592e6d] font-bold oxygen-mono ml-20">
+        <h1 className="text-4xl text-[#592e6d] font-bold oxygen-mono ml-20 mb-16">
           &lt;Projetos/&gt;
         </h1>
         <div className="flex flex-col items-center">
           {projects.map((project, id) => (
-            <div
-              className="my-5 border border-[#bdbdbd] rounded-xl flex w-2/3 p-10 items-center"
+            <motion.div
+              ref={ref}
+              style={{
+                scale: scrollYProgress,
+                opacity: scrollYProgress
+              }}
+              className="my-5 rounded-xl flex w-2/3 p-10 items-center bg-[#ebebeb] relative overflow-hidden"
               key={id}
             >
-              <div className="w-2/3">
-                <Image
-                  src={project.imgSrc}
-                  width={1920}
-                  height={100}
-                  alt="Icone do projeto"
-                ></Image>
-              </div>
               <div className="ml-10 montserrat w-1/3">
-                <h1 className="font-bold text-2xl text-[#592e6d]">
+                <h1 className="font-bold text-3xl text-[#592e6d] mb-3">
                   {project.title}
                 </h1>
                 <p className="text-justify">{project.desc}</p>
-                <div className="flex flex-wrap">
+                <div className="flex flex-wrap mt-2">
                   {project.tags.map((tag, index) => (
                     <span
                       key={index}
-                      className="mr-2 my-1 px-3 py-1 rounded-full bg-[#51cacc]"
+                      className="mr-1 my-1 px-3 py-1 rounded-full bg-[#51cacc]"
                     >
                       {tag}
                     </span>
                   ))}
                 </div>
               </div>
-            </div>
+              <div className="w-2/3">
+                <Image
+                  src={project.imgSrc}
+                  width={1920}
+                  height={100}
+                  alt="Icone do projeto"
+                  className="absolute top-20 -right-40 w-[41rem] rounded-lg shadow-2xl shadow-[#616161] hover:-translate-x-3 hover:translate-y-3 hover:-rotate-2 transition"
+                ></Image>
+              </div>
+            </motion.div>
           ))}
         </div>
       </div>
